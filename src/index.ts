e12e2fe4669fddc521e1155faa0968ef8ed63e32
Will Thomson <will.tho@outlook.com>
2019-07-11 23:13:40 +1200

    Add demo for cloning issue labels to PR

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot'
+import { WebhookPayloadPullRequest, WebhookPayloadPullRequestPullRequest } from '@octokit/webhooks'
+import { IssuesGetResponse, Response } from '@octokit/rest';
+
+type PullRequestComment = {number: any} & {owner: string, repo: string} & { body: string};
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on(['pull_request.opened', 'pull_request.reopened'], async (context: Context) => {
+    const { payload }: { event: string, payload: WebhookPayloadPullRequest } = context;   
+    const { pull_request: pullRequest }: { pull_request: WebhookPayloadPullRequestPullRequest} = payload;
+    const { body }: { body: string } = pullRequest;
+  
+    context.log(body);
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    const issueRegex: RegExp = /\#(\d+)/;
+    const regexMatch: RegExpExecArray | null = issueRegex.exec(body);
+
+    if (regexMatch) {
+      const regexResult: string = regexMatch[1];
+      const number: number = parseInt(regexResult);
+
+      if (number) {
+        context.log(`PR opened for issue #${number}.`);
+
+        const owner: string = "sussol";
+        const repo: string = "msupply";
+        const { data }: Response<IssuesGetResponse> = await context.github.issues.get({owner, repo, number});
+
+        const body = `
+          Beep boop. This PR is associated with issue #${number}.
+
+          Labels: ${data.labels.map(label => label.name).join(", ")}
+        `;
+
+        const pullRequestComment: PullRequestComment = context.issue({ body });
+        await context.github.issues.createComment(pullRequestComment);
+
+      } else {
+        context.log(`PR opened with no or incorrectly formatted issue number.`);
+
+        const body = 'Beep boop. This PR does not have an associated issue!';
+      
+        const pullRequestComment: PullRequestComment = context.issue({ body });
+        await context.github.issues.createComment(pullRequestComment);
+      }
+    }
+  });
 }
