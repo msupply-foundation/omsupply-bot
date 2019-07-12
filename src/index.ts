6f103a05867c416baec518dc1248042f0a1c13f2
Will Thomson <will.tho@outlook.com>
2019-07-12 17:33:21 +1200

    Add prettier, eslint, format code

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,73 @@
 import { Application, Context } from 'probot'
 
 export = (app: Application) => {
-  app.on(['pull_request.opened', 'pull_request.reopened'], async (context: Context) => {
-    const { payload } = context;   
-    const { pull_request } = payload;
-    const { body } = pull_request;
+    app.on(['pull_request.opened', 'pull_request.reopened'], async (context: Context) => {
+        const issueRegex = /\#(\d+)/
+        const issueRegexMatch = issueRegex.exec(context.payload.pull_request.body)
 
-    const issueRegex = /\#(\d+)/;
-    const issueRegexMatch = issueRegex.exec(body);
+        if (issueRegexMatch) {
+            const issueRegexResult = issueRegexMatch[1]
+            const issueNumber = parseInt(issueRegexResult)
 
-    if (issueRegexMatch) {
-      const issueRegexResult = issueRegexMatch[1];
-      const number = parseInt(issueRegexResult);
+            if (issueNumber) {
+                const owner = 'sussol'
+                const repo = 'msupply'
+                const number = issueNumber
 
-      if (number) {
-        const owner = "sussol";
-        const repo = "msupply";
+                const issue = await context.github.issues.get({
+                    owner,
+                    repo,
+                    number,
+                })
+                const labels = issue.data.labels.map(label => label.name)
 
-        const issue = await context.github.issues.get({owner, repo, number});
-        const labels = issue.data.labels.map(label => label.name)
+                const body = `Beep boop. This PR is associated with issue #${number}.`
+                const comment = context.issue({ body })
+                await context.github.issues.createComment(comment)
+                await context.github.issues.addLabels(context.issue({ labels }))
 
-        const body = `Beep boop. This PR is associated with issue #${number}.`;
-
-        const comment = context.issue({ body });
-
-        await context.github.issues.createComment(comment);
-        await context.github.issues.addLabels(context.issue({ labels }));
-
-        const projects = await context.github.projects.listForRepo({ owner, repo});
-        if (projects) {
-          const project = projects.data[0];
-
-          if (project) {
-            const columns = await context.github.projects.listColumns({ project_id: project.id })
-            context.log(columns);
-            if (columns) {
-              const columnDoing = columns.data.find(column => column.name === "In progress");
-              const columnInPR = columns.data.find(column => column.name === "In PR");
-              if (columnDoing && columnInPR) {
-                const cardsDoing = await context.github.projects.listCards({ column_id: columnDoing.id })
-                if (cardsDoing) {
-                  const cardIssue = cardsDoing.data.find(card => card.content_url === issue.data.url);
-                  if (cardIssue) {
-                    await context.github.projects.moveCard({ card_id: cardIssue.id, position: 'top', column_id: columnInPR.id })
-                  }
+                const projects = await context.github.projects.listForRepo({
+                    owner,
+                    repo,
+                })
+                if (projects) {
+                    const project = projects.data[0]
+                    if (project) {
+                        const columns = await context.github.projects.listColumns({
+                            project_id: project.id,
+                        })
+                        if (columns) {
+                            const columnDoing = columns.data.find(column => column.name === 'In progress')
+                            const columnInPR = columns.data.find(column => column.name === 'In PR')
+                            if (columnDoing && columnInPR) {
+                                const cardsDoing = await context.github.projects.listCards({
+                                    column_id: columnDoing.id,
+                                })
+                                if (cardsDoing) {
+                                    const cardIssue = cardsDoing.data.find(card => card.content_url === issue.data.url)
+                                    if (cardIssue) {
+                                        const { id: card_id } = cardIssue
+                                        const { id: column_id } = columnInPR
+                                        await context.github.projects.moveCard({
+                                            card_id,
+                                            position: 'top',
+                                            column_id,
+                                        })
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-              }
+
+                context.log(`PR opened for issue #${number}.`)
+            } else {
+                const body = 'Beep boop. This PR does not have an associated issue!'
+                const comment = context.issue({ body })
+                await context.github.issues.createComment(comment)
+
+                context.log(`PR opened with no or incorrectly formatted issue number.`)
             }
-          }
         }
-
-        context.log(`PR opened for issue #${number}.`);
-      } else {
-        const body = 'Beep boop. This PR does not have an associated issue!';
-      
-        const comment = context.issue({ body });
-
-        await context.github.issues.createComment(comment);
-
-        context.log(`PR opened with no or incorrectly formatted issue number.`);
-      }
-    }
-  });
+    })
 }
