7c2cf88e5a69c093b60f29a600e0847e62b34367
Will Thomson <will.tho@outlook.com>
2019-09-21 21:53:12 +1200

    Fix up initial test and fixtures

--- test/pullRequest.test.ts
+++ test/pullRequest.test.ts
@@ -4,7 +4,10 @@ import nock from 'nock';
 import App from '../src';
 import { ERRORS } from '../src/constants';
 
-import { opened as pullRequestOpenedPayload } from './fixtures/pullRequest.json';
+import { 
+  pullRequestOpenedWithoutIssue,
+  pullRequestOpenedWithIssue
+} from './fixtures/pullRequest';
 
 nock.disableNetConnect();
 
@@ -18,10 +21,11 @@ describe('pull request opened', () => {
   });
 
   test('create a comment if a pull request has no linked issue', async done => {
+    const { payload: pullRequestOpenedNoIssuePayload } = pullRequestOpenedWithoutIssue;
     const {
       pull_request: pullRequest,
       repository: pullRequestRepository,
-    } = pullRequestOpenedPayload;
+    } = pullRequestOpenedNoIssuePayload;
     const { number: pullRequestNumber } = pullRequest;
     const { name: repositoryName, owner: repositoryOwner } = pullRequestRepository;
     const { login: repositoryOwnerName } = repositoryOwner;
@@ -29,20 +33,25 @@ describe('pull request opened', () => {
     // Test bot makes GET request for PR repo.
     nock('https://api.github.com')
       .get(`/repos/${repositoryOwnerName}/${repositoryName}?number=${pullRequestNumber}`)
-      .reply(200, pullRequestRepository);
+      .reply(200, pullRequestRepository)
+      .log(console.log);
 
     // Test bot makes POST request to update PR comment.
     nock('https://api.github.com')
       .post(
         `/repos/${repositoryOwnerName}/${repositoryName}/issues/${pullRequestNumber}/comments`,
         (payload: object) => {
-          done(expect(payload).toMatchObject({ body: ERRORS.NO_LINKED_ISSUE }));
+          expect(payload).toMatchObject({ body: ERRORS.NO_LINKED_ISSUE });
           return true;
         }
       )
-      .reply(200);
+      .reply(200)
+      .log(console.log);
+
+    // Pass test if all mocked APIs are called and bot posts correct error comment to PR.
+    done(nock.isDone())
 
     // Send bot a webhook event for a new PR without a linked issue.
-    await probot.receive({ name: 'pull_request', payload: pullRequestOpenedPayload });
+    await probot.receive({ name: 'pull_request', payload: pullRequestOpenedNoIssuePayload });
   });
 });
