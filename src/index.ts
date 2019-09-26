98b3b04e25876c3247d3a9dbfa3a12df0bcee3b8
Will Thomson <will.tho@outlook.com>
2019-09-26 20:24:18 +1200

    Add files destructed by bad merge

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { Application } from 'probot';
 
+import issue from './issue';
 import pullRequest from './pullRequest';
+import release from './release';
 
 const events = {
+  issueAssigned: 'issues.assigned',
+  issueLabelled: 'issues.labeled',
   pullRequestOpened: ['pull_request.opened', 'pull_request.reopened'],
+  releaseCreated: ['release.created', 'release.edited'],
 };
 
 export = (app: Application) => {
+  app.on(events.issueAssigned, issue.assigned);
+  app.on(events.issueLabelled, issue.labelled);
   app.on(events.pullRequestOpened, pullRequest.opened);
-  // app.on(events.releaseCreated, release.created);
+  app.on(events.releaseCreated, release.created);
 };
---
+++ src/release.ts
@@ -0,0 +1,13 @@
+import { Context } from 'probot';
+import { GitHubAPI } from 'probot/lib/github';
+import { WebhookPayloadRelease, WebhookPayloadReleaseRelease } from '@octokit/webhooks';
+import { updateBuildSpec } from './jenkins';
+
+export const created = async (context: Context) => {
+  const { payload }: { github: GitHubAPI; payload: WebhookPayloadRelease } = context;
+  const { release }: { release: WebhookPayloadReleaseRelease } = payload;
+  const { tag_name: buildTag }: { tag_name: string } = release;
+  await updateBuildSpec(buildTag);
+};
+
+export default { created };
