b8c0df99f611a5fa9bce519cd0db6dd35bc5489b
Will Thomson <will.tho@outlook.com>
2019-11-18 14:27:40 +0545

    Add release test

--- src/events/release.ts
+++ src/events/release.ts
@@ -1,6 +1,5 @@
 import { Context } from 'probot';
 import { GitHubAPI } from 'probot/lib/github'
-import { updateBuildSpec } from '../helpers/jenkins';
 import { ReleasePayload } from '../types';
 import { Release } from '../types';
 
@@ -8,7 +7,7 @@ export const created = async (context: Context) => {
   const { payload }: { github: GitHubAPI; payload: ReleasePayload } = context;
   const { release }: { release: Release } = payload;
   const { tag_name: buildTag }: { tag_name: string } = release;
-  await updateBuildSpec(buildTag);
+  console.log(buildTag);
 };
 
 export default { created };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Application } from 'probot';
-import { issue, pullRequest, projectCard } from './events';
+import { issue, pullRequest, projectCard, release } from './events';
 
 const events = {
   issueAssigned: 'issues.assigned',
@@ -13,6 +13,6 @@ export = (app: Application) => {
   app.on(events.issueAssigned, issue.assigned);
   app.on(events.issueLabelled, issue.labelled);
   app.on(events.pullRequestOpened, pullRequest.opened);
-  // app.on(events.releaseCreated, release.created);
+  app.on(events.releaseCreated, release.created);
   app.on(events.projectCardMoved, projectCard.moved);
 };
